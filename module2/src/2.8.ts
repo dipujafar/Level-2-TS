397fbb1b6708591446c164cc3d42ddd0237d9da7
dipujafar <dipujafarmr@gmail.com>
2025-08-11 00:38:48 +0600

    added

---
+++ module2/src/2.8.ts
@@ -0,0 +1,19 @@
+{
+  const createPromise = () => {
+    return new Promise((resolve, reject) => {
+      const data: string = "something";
+      if (data) {
+        resolve(data);
+      } else {
+        reject("failed to load data");
+      }
+    });
+  };
+
+  const showData = async () => {
+    const data = await createPromise();
+    console.log(data);
+  };
+
+  showData();
+}
